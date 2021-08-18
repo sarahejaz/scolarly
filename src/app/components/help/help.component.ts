6c2f749587173199150c1479b16fcb28e2fa95bc
Sarah <58132791+sarahejaz@users.noreply.github.com>
2021-08-18 15:08:57 +0500

    Added Help files

---
+++ src/app/components/help/help.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-help',
+  templateUrl: './help.component.html',
+  styleUrls: ['./help.component.css']
+})
+export class HelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  goHome() {
+    window.open("http://localhost:4200/home", "_self");
+  }
+
+}
