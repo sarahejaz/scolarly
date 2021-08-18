68a0f56186eb4877cb9ff9b8cb544db1e5adf9d3
Sarah <58132791+sarahejaz@users.noreply.github.com>
2021-08-18 15:04:30 +0500

    Create edit-assignment.component.spec.ts

---
+++ src/app/components/assignment/edit-assignment/edit-assignment.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditAssignmentComponent } from './edit-assignment.component';
+
+describe('EditAssignmentComponent', () => {
+  let component: EditAssignmentComponent;
+  let fixture: ComponentFixture<EditAssignmentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditAssignmentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditAssignmentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
