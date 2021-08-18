03c994ad2ed5692f5be971119807d06d5d2db773
Sarah <58132791+sarahejaz@users.noreply.github.com>
2021-08-18 15:07:24 +0500

    Create view-assignment.component.spec.ts

---
+++ src/app/components/assignment/view-assignment/view-assignment.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewAssignmentComponent } from './view-assignment.component';
+
+describe('ViewAssignmentComponent', () => {
+  let component: ViewAssignmentComponent;
+  let fixture: ComponentFixture<ViewAssignmentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewAssignmentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewAssignmentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
